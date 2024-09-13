ca877263bbe54efd4b4a056685d9ab12f5c3a727
Engin Yıldız <enginyildiz33@gmail.com>
2024-09-13 20:15:46 +0300

    Tailwindcss Sectors

---
+++ TailwindSektorler/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.{html,js}",
+    "./public/**/*.html",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
